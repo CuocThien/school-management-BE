b95ff5d48372a67a02327d5e24a330b23b9a8703
Cước Thiên <nguyenthien@escorti.com.vn>
2023-09-23 23:17:31 +0700

    feat: init source backend

---
+++ libs/utils/index.ts
@@ -0,0 +1,5 @@
+export * from './class-transform';
+export * from './class-validator';
+export * from './function';
+export * from './constant';
+export * from './interfaces';
---
+++ src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { RequestContextModule } from 'nestjs-request-context';
+import { HttpGuard } from 'libs/middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfigService } from './config/jwt-config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    RequestContextModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      // entities: '../libs/entities/index.ts',
+      synchronize: false,
+    }),
+    TypeOrmModule.forFeature([]),
+    JwtModule.registerAsync({
+      useClass: JwtConfigService,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: HttpGuard,
+    },
+  ],
+})
+export class AppModule {}
